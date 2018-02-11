dee6c127a50a0541fbe99567f850b70350dbafb6
Axel Rutz <axel.rutz@clever-systems.net>
2018-02-11 19:15:41 +0100

    Fixed notice

--- symlink8/settings/server-freistilbox.php
+++ symlink8/settings/server-freistilbox.php
@@ -12,7 +12,7 @@ $isLive = strpos($siteurl, '://test.') !== FALSE;
 
 // Redirect to HTTP(S) if necessary.
 if (PHP_SAPI !== 'cli') {
-  if ($_SERVER['HTTPS'] !== "on") {
+  if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== "on") {
     if (file_exists($_SERVER['HOME'] . '/ssl')) {
     $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
