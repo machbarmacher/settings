a0180c88cc592c4eeaee34e70fee00e9cdb36217
Axel Rutz <axel.rutz@clever-systems.net>
2018-02-14 15:40:26 +0100

    Updated drushrc.php

--- scaffold/drush/drushrc.php
+++ scaffold/drush/drushrc.php
@@ -1,3 +1,13 @@
 <?php
 $options['l'] = require '../settings/generated/local/siteurl.php';
 
+// Leave out unnecessary DB data in sql-sync and sql-dump.
+$options['structure-tables']['common'] = array('cache', 'cache_*', 'cachetags', 'history', 'sessions', 'watchdog');
+$options['structure-tables-key'] = 'common';
+
+$options['skip-tables']['common'] = array('migrate_*');
+$options['skip-tables-key'] = 'common';
+
+// Leave out unnecessary files.
+$command_specific['deploya-pull-data']['public-files-exclude-paths'] = 'advagg_css/:advagg_js/:css/:ctools/:feeds/:js/:languages/:styles/';
+
