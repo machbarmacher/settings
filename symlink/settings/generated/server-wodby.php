33ebc098349b00e7122247723f816f45c4db4e09
Axel Rutz <axel.rutz@clever-systems.net>
2018-02-27 03:14:23 +0100

    Fixed server-wodby

--- symlink/settings/generated/server-wodby.php
+++ symlink/settings/generated/server-wodby.php
@@ -1,6 +1,6 @@
 <?php
-$isLive = WODBY_INSTANCE_TYPE === 'prod';
-$siteurl = $WODBY_URL_PRIMARY;
+$isLive = getenv('WODBY_INSTANCE_TYPE') === 'prod';
+$siteurl = getenv('WODBY_URL_PRIMARY');
 // We include this here, rather than relying on wodby adding to s/d/settings.php.
 include '/var/www/conf/wodby.settings.php';
 
