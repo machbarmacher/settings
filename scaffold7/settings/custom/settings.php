1d4b9135fe1a6fdd26b31ac2f05e9850f74ee71b
Axel Rutz <axel.rutz@clever-systems.net>
2018-02-28 20:41:33 +0100

    Added d7 master scaffold

--- scaffold7/settings/custom/settings.php
+++ scaffold7/settings/custom/settings.php
@@ -6,6 +6,20 @@ if (!empty($isLive)) {
   $conf['shield_enabled'] = FALSE;
 }
 
+// Set master scope.
+$conf['master_version'] = 2;
+$conf['master_modules'] = array(
+  'base' => array(
+    // 'XXX',
+  ),
+  'dev' => array(),
+  'live' => array(),
+);
+$conf['master_removable_blacklist'] = array(
+  0 => 'modules/*',
+);
+$conf['master_current_scope'] = $isLive ? 'live' : 'dev';
+
 // Set solr server for index with name "content". The required file is auto-symlinked.
 // @todo Figure out how to mangle the server name into the array keys.
 // $conf += array_map(..., require '../settings/solr.php');
