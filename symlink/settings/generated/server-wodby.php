fe8cb3d39aee048c514f882aee6aa9318cbc216d
Axel Rutz <axel.rutz@clever-systems.net>
2018-04-13 02:17:09 +0200

    Added wodby umask

--- symlink/settings/generated/server-wodby.php
+++ symlink/settings/generated/server-wodby.php
@@ -3,4 +3,5 @@ $isLive = getenv('WODBY_INSTANCE_TYPE') === 'prod';
 $siteurl = getenv('WODBY_URL_PRIMARY');
 // We include this here, rather than relying on wodby adding to s/d/settings.php.
 include '/var/www/conf/wodby.settings.php';
-
+// If www-data writes files, wodby should own them too.
+umask(002);
