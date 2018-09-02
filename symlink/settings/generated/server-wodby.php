1fdeda36fed7a91faace523a996377d15d49609a
Axel Rutz <axel.rutz@machbarmacher.net>
2018-09-03 00:01:35 +0200

    Fixed km/#19 public files permissions

--- symlink/settings/generated/server-wodby.php
+++ symlink/settings/generated/server-wodby.php
@@ -6,3 +6,11 @@ $instanceName = getenv('WODBY_INSTANCE_NAME');
 include '/var/www/conf/wodby.settings.php';
 // If www-data writes files, wodby should own them too.
 umask(002);
+// Set sgid so dirs written by drush from wodby user inherit www-data group.
+if ((fileperms('sites/default/files/') & 07777) !== 02775) {
+  chmod('sites/default/files/', 02775);
+}
+// Set dir mask accordingly.
+$settings['file_chmod_directory'] = 02775;
+// This is standard.
+$settings['file_chmod_file'] = 0664;
